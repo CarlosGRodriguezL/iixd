9073b47539234a1e5d40c18500206f5764c0f844
Carlos <crolo@gmx.at>
2016-12-16 00:34:35 +0100

    some changes

--- script.js
+++ script.js
@@ -7,25 +7,47 @@ $(document).ready(function(){
 	$("#ISBN").keyup(function(){
 		if(this.value){
 			$(".books").removeClass("hide");
+			$(".scripts").addClass("hide");
 		}else{
 			$(".books").addClass("hide");
+			$(".books").addClass("hide");
+			$("#ISBN").val ("");
+			$("#Author").val ("");
+			$("#titel").val ("");
+			$("#Studienrichtung").val ("");
+			$("#Fächer").val ("");
 		}
 	});
 	$("#titel").keyup(function(){
 		if(this.value){
 			$(".scripts").removeClass("hide");
+			$(".books").addClass("hide");
 		}else{
 			$(".scripts").addClass("hide");
+			$(".books").addClass("hide");
+			$("#ISBN").val ("");
+			$("#Author").val ("");
+			$("#Studienrichtung").val ("");
+			$("#Fächer").val ("");
 		}
 	});
 	
 	$(".list-group-item").click(function(){
 		$(".list-group-item").removeClass("active");
 		$(this).addClass("active");
-		$("#ISBN").val ("10....");
-		$("#Author").val ("Hans Peter");
-		$("#titel").val ("Informatik ...");
-		$("#Studienrichtung").val ("Informatik");
-		$("#Fächer").val ("PK, OOP, IIXD,...");
+		if($(this).closest(".books").length>0){
+			$("#ISBN").val ("10....");
+			$("#Author").val ("Hans Peter");
+			$("#titel").val ("Informatik Grundlagen ...");
+			$("#Studienrichtung").val ("Informatik");
+			$("#Fächer").val ("PK, OOP, IIXD,...");
+		}else{
+			$("#ISBN").val ("");
+			$("#Author").val ("Jörg Maier");
+			$("#titel").val ("Informatik Basics Script ...");
+			$("#Studienrichtung").val ("Informatik");
+			$("#Fächer").val ("PK, OOP, IIXD,...");
+		}
+		
 	});
 });
